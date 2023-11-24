82905c5c037d51ef5da18dbdeba665551c8a29f1
Tunde Sanusi <52820977+tuhamworld@users.noreply.github.com>
2023-11-24 19:57:16 +0100

    feat: Complete UseContext

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { createContext, useContext, useState } from "react";
 // import UserProfileCard from './components/UserProfileCard';
 // import ListComponent from './components/ListComponent';
 // import Counter from './components/counter';
@@ -12,52 +12,87 @@ import React from "react";
 // import UseContext from "./components/UseContext";
 import "./index.css";
 
-// create Context
+// Creating Context
+
 const AuthContext = createContext({
-  isAuthenticated: false,
   user: {
     username: "tuhamworld",
     name: "Tuham",
     email: "example@gmail.com",
   },
+  verified: false,
 });
 
 // Define Context Provider
 function AuthProvider(props) {
+  const [user, setUser] = useState({
+    username: "tuhamworld",
+    name: "Tunde Sanusi",
+    email: "example@gmail.com",
+    bio: "Tunde Sanusi is passionate about Building online presence for Businesess via Technological Solutions",
+  });
   return (
-    <AuthContext.provider value={
-      {
-        isAuthenticated: false,
-        user: {
-          username: "tuhamworld",
-          name: "Tuham",
-          email: "example@gmail.com",
-        },
-      }
-
-      }>
+    <AuthContext.Provider
+      value={{
+        user: user,
+        verified: true,
+        setUser: setUser,
+      }}
+    >
       {props.children}
-    </AuthContext.provider>
+    </AuthContext.Provider>
+  );
+}
+
+// User Bio
+function UserBio() {
+  const { user, setUser } = useContext(AuthContext);
+
+  const handleUpdateBio = (event) => {
+    // Update Bio
+    event.preventDefault();
+    setUser((prev) => {
+      return {
+        ...prev,
+        bio: "Tunde Sanusi was born in Ibadan, Nigeria.",
+      };
+    });
+  };
+
+  return (
+    <div>
+      <h1>User Bio</h1>
+      <div>{user.bio}</div>
+      <button onClick={handleUpdateBio}>Update Bio</button>
+    </div>
   );
 }
 
 // User Profile
 function UserProfile() {
+  const { user, verified } = useContext(AuthContext);
+  console.log(user);
+
   return (
     <div>
       <h1>User Profile</h1>
-      <div>Username: tuhamworld</div>
-      <div>Name: Tunde Sanusi</div>
-      <div>Email: example@gmail.com</div>
+      <div>Username: {user.username}</div>
+      <div>Name: {user.name}</div>
+      <div>Email: {user.email}</div>
+      <div>Verified: {verified ? "Yes" : "No"}</div>
+
+      <UserBio />
     </div>
   );
 }
 
 function App() {
   return (
-    <div className="main-page">
-      <UserProfile />
-    </div>
+    <AuthProvider>
+      <div className="main-page">
+        <UserProfile />
+      </div>
+    </AuthProvider>
   );
 }
 export default App;
