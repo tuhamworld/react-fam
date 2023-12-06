1c6ca49981f6b52eb47393caf363b0c6ba411006
Tunde Sanusi <52820977+tuhamworld@users.noreply.github.com>
2023-12-06 08:17:35 +0100

    feat: routes added successfully

---
+++ src/components/pages/LandingPage/index.js
@@ -0,0 +1,17 @@
+export const LandingPage = () => {
+  return (
+    <section>
+      <h1>Welcome to the Landing Page</h1>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem
+      nesciunt, nihil fugiat illum tempore temporibus esse laudantium, aperiam
+      facilis ab dicta magnam minus assumenda eveniet a, vitae odio quae nulla.
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat optio,
+      deleniti, asperiores autem accusamus voluptatum ut quidem accusantium
+      recusandae quas consequatur molestias? Et sequi minima numquam, veritatis
+      aspernatur voluptatum alias. Lorem ipsum dolor sit amet consectetur
+      adipisicing elit. Nam vitae eaque ducimus amet suscipit sequi eos, magni
+      quibusdam unde cum! Molestias, saepe sit. Voluptas, perferendis odio
+      architecto praesentium error dolorum.
+    </section>
+  );
+};
