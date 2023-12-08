42c82e6830e787b65dcb38504ecf879b32471f9a
Tunde Sanusi <52820977+tuhamworld@users.noreply.github.com>
2023-12-08 08:48:48 +0100

    chore: Redirect users w/ Authentication

--- src/components/pages/Dashboard/index.js
+++ src/components/pages/Dashboard/index.js
@@ -1,9 +1,18 @@
+import { useState } from "react";
+import { Navigate } from "react-router-dom";
+
 export const Dashboard = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
   return (
     <section>
-          <h1>This is a Dashboard</h1>
+      {isAuthenticated ? (
+        <>
+        <h1>This is a Dashboard</h1>
           <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate cum, aliquam magni, facilis suscipit molestiae autem ipsa ipsum corporis voluptatum repellendus blanditiis sit ducimus, praesentium maxime quisquam placeat. Quibusdam, omnis. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Adipisci voluptates sapiente velit laudantium itaque facilis reprehenderit voluptatem quas officia repudiandae, ipsa illo amet libero aut pariatur natus est minima error.</div>
-
+        </>
+      ) : (
+          <Navigate to="/registration" replace={true} />
+      )}
     </section>
   );
 };
--- src/components/pages/Users/userDetails.js
+++ src/components/pages/Users/userDetails.js
@@ -1,7 +1,13 @@
+import { useParams, useSearchParams } from "react-router-dom"
+
 export const UserDetails = () => {
+    const { userId } = useParams();
+    const  [searchParams] = useSearchParams();
     return (
         <section>
             <h1>This is a User Details</h1>
+            <h2>USER ID: { userId }</h2>
+            <h2>Query: { searchParams.get("name")} and { searchParams.get("email")} </h2>
             <div>
                 Lorem, ipsum dolor sit amet consectetur adipisicing elit. Expedita labore quis, mollitia id, est provident esse tempore quae nihil sit officia et reprehenderit culpa, repellat harum! Sed numquam at adipisci. Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque sint quam voluptate, officiis libero ut exercitationem, nisi quae quidem perferendis, accusantium necessitatibus quia tempora rem quo culpa voluptas dolor obcaecati!
             </div>
