b19d1de9f0e72c552d1f11f5b560ee7ca70426e9
Tunde Sanusi <52820977+tuhamworld@users.noreply.github.com>
2023-12-23 05:28:41 +0100

    feat: added Pagination

--- src/components/pages/Pagination/index.js
+++ src/components/pages/Pagination/index.js
@@ -1,20 +1,95 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import "./index.scss";
 
-function Pagination() {
+export const Pagination = () => {
+  // Setting up our state
+  const [currentPosition, setCurrentPosition] = useState(0);
+  const [news, setNews] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [listItems, setListItems] = useState([]);
+
+  // Setting up useEffect
+  useEffect(() => {
     const newsItem = [
-        {
-            id: "1",
-            title: "News Items 1",
-            description: "This is the first news Item",
-            url: "http://google.com",
-            urlToImage: "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png"
-        }
-    ]
-    return (
-        <div>
+      {
+        title: "1. BBC",
+        url: "http://bbc.com",
+        description: "Latest News from BBC",
+      },
+      {
+        title: "2. CNN",
+        url: "https://cnn.com",
+        description: "Latest News from CNN",
+      },
+      {
+        title: "3. Google News",
+        url: "https://news.google.com",
+        description: "Latest News from Google",
+      },
+      {
+        title: "4. TechCrunch",
+        url: "https://techcrunch.com",
+        description: "Latest News from TechCrunch",
+      },
+      {
+        title: "5. The Verge",
+        url: "https://theverge.com",
+        description: "Latest News from the Verge",
+      },
+    ];
+    const item = newsItem[currentPosition];
+    setListItems(newsItem);
+    // Set our state..
+    setNews(item);
+    setLoading(false);
+  }, [currentPosition]);
 
-        </div>
-    )
-}
+  // Handle Next Button...
+  const handleNext = (event) => {
+    event.preventDefault();
+    // Use our state....
+    // setCurrentPosition(prev => prev + 1);
 
-export default Pagination;
+    const length = listItems.length;
+    const count = length - 1;
+
+    if (currentPosition < count - 1) {
+      setCurrentPosition(currentPosition + 1);
+    } else {
+      setCurrentPosition(0);
+    }
+  };
+
+  // Handle Previous...
+  const handlePrevious = (event) => {
+    event.preventDefault();
+    // Use our state
+    // setCurrentPosition(prev => prev - 1);
+    if (currentPosition > 0) {
+      setCurrentPosition((prev) => prev - 1);
+    }
+  };
+
+  return (
+    <>
+      <section className="news">
+        {loading ? (
+          <div>Loading... </div>
+        ) : (
+          <div className="news-item">
+            <h2>{news.title}</h2>
+            <p>{news.description}</p>
+            <a href="{news.url}">{news.url}</a>
+          </div>
+        )}
+      </section>
+      <div className="paginate"></div>
+      <button className="prev" onClick={handlePrevious}>
+        Prev
+      </button>
+      <button className="next" onClick={handleNext}>
+        Next
+      </button>
+    </>
+  );
+};
