3a9658e89fb115e82cc446157c9d5a8d8ddac286
Tunde Sanusi <52820977+tuhamworld@users.noreply.github.com>
2023-12-24 10:59:59 +0100

    refactor: one liner without return

--- src/components/molecules/Avatar/index.js
+++ src/components/molecules/Avatar/index.js
@@ -1,18 +1,14 @@
 import React from "react";
 import "./index.scss";
 
-const Avatar = () => {
-  return (
-    <>
-      <img
-        className="card-image"
-        src="https://w3schools.com/howto/img_avatar.png"
-        alt="user_avatar"
-        width="200px"
-        height="200px"
-      />
-    </>
-  );
-};
+const Avatar = () => (
+  <img
+    className="card-image"
+    src="https://w3schools.com/howto/img_avatar.png"
+    alt="user_avatar"
+    width="200px"
+    height="200px"
+  />
+);
 
 export default Avatar;
