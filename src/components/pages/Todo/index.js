aa994d0030b97d40ac6a740a4711054866a45fdb
Tunde Sanusi <52820977+tuhamworld@users.noreply.github.com>
2023-12-30 12:27:12 +0100

    feat: adding todo-list

---
+++ src/components/pages/Todo/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Todo = () => {
+    return (
+        <section>
+            <form>
+                <input type="text" placeholder="Enter your to-do" />
+                <button>Add</button>
+            </form>
+        </section>
+    )
+}
+
+export default Todo;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import App from './App';
+import App from './App';
 import './index.css';
-// import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { LandingPage } from './components/pages';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <BrowserRouter>
+    <BrowserRouter>
     <App />
-    </BrowserRouter> */}
+    </BrowserRouter>
     <LandingPage/>
   </React.StrictMode>
 );
